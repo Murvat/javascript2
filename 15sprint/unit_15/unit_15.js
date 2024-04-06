c72543e40601bb810c8f1f486e8a6a7afa457765
Murvat <murvat.mustafayev@mail.ru>
2024-04-06 18:43:32 +0100

    15sprintFinish

--- 15sprint/unit_15/unit_15.js
+++ 15sprint/unit_15/unit_15.js
@@ -3,16 +3,26 @@
 // Task 1
 // Создайте set s1. Добавьте в него три элемента 'h', 'b', 'o', 'h'. Добавление делайте через add. Выведите в консоль получившийся набор (set) s1.
 
-// let s1 = new ...
+let s1 = new Set();
+s1.add('h')
+s1.add('b')
+s1.add('o')
+s1.add('h')
+
+console.log(s1)
 
 
 // Task 2
 // При нажатии b-2 выполняете функцию f2. Функция должна добавить в набор s2 элементы, которые пользователь вводит в i-2. Функция должна выводить в консоль s2 после каждого добавления элемента.
 
 
-// let s2 =
+let s2 = new Set();
 
-const f2 = () => { }
+const f2 = () => {
+    let inData = document.querySelector('.i-2');
+    s2.add(inData.value);
+    console.log(s2)
+}
 
 document.querySelector('.b-2').onclick = f2;
 
@@ -22,7 +32,11 @@ document.querySelector('.b-2').onclick = f2;
 
 let s3 = new Set(['one', 'two', 'four']); // обратите внимание, как просто можно добавить массив в набор!
 
-const f3 = () => { }
+const f3 = () => {
+    let inData = document.querySelector('.i-3');
+    s3.delete(inData.value);
+    console.log(s3)
+}
 
 document.querySelector('.b-3').onclick = f3;
 
@@ -32,7 +46,15 @@ document.querySelector('.b-3').onclick = f3;
 
 let s4 = new Set(['a', 'b', 'c', 'z']);
 
-const f4 = () => { }
+const f4 = () => {
+    let inData = document.querySelector('.i-4');
+    if (s4.has(inData.value)) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+
+}
 
 document.querySelector('.b-4').onclick = f4;
 
@@ -42,7 +64,10 @@ document.querySelector('.b-4').onclick = f4;
 
 let s5 = new Set(['a', 'b', 'c', 'z', 'a2', 'b2', 'c2', 'z2']);
 
-const f5 = () => { }
+const f5 = () => {
+    let out = document.querySelector('.out-5');
+    out.innerHTML = s5.size;
+}
 
 document.querySelector('.b-5').onclick = f5;
 
@@ -51,14 +76,30 @@ document.querySelector('.b-5').onclick = f5;
 
 let a6 = [1, 2, 3, 4, 5, 3, 4, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56, 4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56];
 
-const f6 = () => { }
+const f6 = () => {
+    s6 = new Set(a6)
+    let out = document.querySelector('.out-6');
+    out.innerHTML = s6.size;
+
+}
 
 document.querySelector('.b-6').onclick = f6;
 
 // Task 7
 // При нажатии b-7 выполняете функцию f7. Функция должна получать из i-7 значение пароля и проверять, чтобы пользователь в строке пароля использовал не повторяющиеся символы. Если символы уникальны, а длина пароля больше ( строго) 6 то выводите в out-7 число 1. Если есть повторяющиеся символы, или длина меньше или равна 6 - то выводите 0. Для проверки уникальности символов используйте Set.
 
-const f7 = () => { }
+const f7 = () => {
+    let out = document.querySelector('.out-7');//сюда выводим
+    let inData = document.querySelector('.i-7').value//отсюда получаем
+    s7 = new Set(inData.split(''))//разделяем на символы
+    console.log(s7)
+    if (inData.length === s7.size && s7.size > 6) {
+        out.innerHTML = 1;
+    } else {
+        out.innerHTML = 0;
+
+    }
+}
 
 document.querySelector('.b-7').onclick = f7;
 
@@ -68,7 +109,13 @@ document.querySelector('.b-7').onclick = f7;
 let s8 = new Set([1, 2, 3, 4, 5, 3, 4, 7, 9, 5, 7, 8, 9, 23, 45, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56, 4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56]);
 let ar8 = [];
 
-const f8 = () => { }
+const f8 = () => {
+    for (let item of s8) {
+        if (item > 5) { ar8.push(item) };
+    }
+    console.log(ar8)
+
+}
 
 document.querySelector('.b-8').onclick = f8;
 
@@ -76,17 +123,27 @@ document.querySelector('.b-8').onclick = f8;
 //  При нажатии b-9 выполняете функцию f9. Функция должна принимать набор our_set в качестве параметра, преобразовывать его в строку, причем после каждого символа строки должен быть пробел. Функция должна возвращать результирующую строку. 
 // В нашем примере результат должен быть 9 8 7 6 5 
 
-const f9 = our_set => { }
+const f9 = our_set => {
+    let newS = Array.from(our_set);
+    const result = newS.join(' ');
+    return result;
+}
 
 document.querySelector('.b-9').onclick = () => {
     let s9 = new Set([9, 8, 7, 6, 5]);
+
     document.querySelector('.out-9').innerHTML = f9(s9);
 }
 
 // Task 10
 // При нажатии b-10 выполняете функцию f10. Функция должна принимать набор set в качестве параметра и выводить его в указанный элемент. Элемент указывается как второй параметр функции f10. Вывод значений - через пробел.
 
-const f10 = (out_set, elem) => { }
+const f10 = (out_set, elem) => {
+    let newArr = Array.from(out_set);
+    let result = newArr.join(' ')
+    let out = document.querySelector(elem);
+    out.innerHTML = result;
+}
 
 document.querySelector('.b-10').onclick = () => {
     let a10 = new Set(['4', '5', '6']);
@@ -113,6 +170,13 @@ document.querySelector('.b-11').onclick = f11;
 let str12 = 'The name conjures up visions of plum pudding and Christmas punch quaint coaching inns and cozy firesides but also of orphaned and starving children';
 
 const f12 = () => {
+    let arr = str12.split(' ');
+
+    let arr2 = str12.split('');
+    let newSet = new Set(arr2);
+    return newSet;
+
+
 
 }
 
@@ -131,8 +195,19 @@ let str13 = 'abbat pro';
 
 
 const f13 = () => {
+    let res = {};//Для начала создаем временный объект:
+    let s13 = new Set(str13);//Потом создаем набор из строки - это сразу разобьет ее по символам:
+    console.log(s13);
+    for (let s of s13) {//Мы получаем символ который будем считать в строке.
+        let count = 0;
+        for (let i = 0; i < str13.length; i++) {//Теперь задача - посчитать сколько раз он встречается в строке 
+            if (s === str13[i]) count++;// // проверяем совпадает ли текущий символ строки
+            // с s. Если да - добавляе 1 к счетчику
+        }
+        res[s] = count;// нужно записать символ s и его количество в строке count в массив res
 
-    // return
+    }
+    return res
 }
 
 document.querySelector('.b-13').onclick = () => {

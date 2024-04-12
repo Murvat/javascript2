292107f5b8d7905bb751532b161800766ead735f
Murvat <murvat.mustafayev@mail.ru>
2024-04-12 19:43:43 +0100

    22sprintFinish

--- 22sprint/unit_22/unit_22.js
+++ 22sprint/unit_22/unit_22.js
@@ -3,9 +3,13 @@
 
 
 function t1() {
-    // тут добавляете try
-    let a = 22;
-    let c = a + d;
+    try {
+        let a = 22;
+        let c = a + d;
+
+    } catch (error) {
+        document.querySelector('.out-1').innerHTML = 1;
+    }
     // тут catch
     // .. и вывод
 }
@@ -16,9 +20,13 @@ document.querySelector('.b-1').onclick = t1;
 // Добавьте в код функции try catch так, чтобы вместо ошибки был вывод результата в out-2.
 
 function t2() {
-    let a = 4;
-    let b = 5;
-    document.querySelector('.out-2222222').innerHTML = a*b;
+    try {
+        let a = 4;
+        let b = 5;
+        document.querySelector('.out-2222222').innerHTML = a * b;
+    } catch (error) {
+        document.querySelector('.out-2').innerHTML = error.message;
+    }
 
 }
 
@@ -30,21 +38,34 @@ document.querySelector('.b-2').onclick = t2;
 // т.е. вы не знаете будет или нет он в html.
 
 function t3() {
-    let a = 4;
-    let b = 5;
-    document.querySelector('.out-3').innerHTML = a*b;
+    try {
+        let a = 4;
+        let b = 5;
+        document.querySelector('.out-3').innerHTML = a * b;
+    } catch (error) {
+        document.querySelector('.out-3').innerHTML = error.message;
+    }
 }
 
 document.querySelector('.b-3').onclick = t3;
 
 //  Task 4
 // Дана переменная a. В переменную делается push. Используя try catch отловите ошибки если они есть. Если ошибка вывести в out-4 число 0. Если не ошибка - то результирующий массив через пробел.
-
-let a = [2,3,4];
-// a = 5;
-
-function t4() {
-    a.push(7);
+try {
+    let a = [2, 3, 4];
+    function t4() {
+        a.push(7);
+    }
+    t4();
+} catch (error) {
+    document.querySelector('.out-4').innerHTML = 0;
+} finally {
+    let a = [2, 3, 4];
+    let out = '';
+    for (let i = 0; i < a.length; i++) {
+        out += a[i] + ' ';
+    }
+    document.querySelector('.out-4').innerHTML = out;
 }
 
 document.querySelector('.b-4').onclick = t4;
@@ -54,8 +75,14 @@ document.querySelector('.b-4').onclick = t4;
 // Добавьте try, catch, finnaly так, чтобы в out-5 выводился 0 при ошибки. А в out-5-1 всегда выводилось слово 'finnaly';
 
 function t5() {
-    let p = document.querySelectorAll('p');
-    p.push(3);
+    try {
+        let p = document.querySelectorAll('p');
+        p.push(3);
+    } catch (error) {
+        document.querySelector('.out-5').innerHTML = 0
+    } finally {
+        document.querySelector('.out-5-1').innerHTML = 'finally'
+    }
 }
 
 document.querySelector('.b-5').onclick = t5;

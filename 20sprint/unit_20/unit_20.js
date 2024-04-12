fdecf6ca22d099a0a801bd98cf44e99ce72535c1
Murvat <murvat.mustafayev@mail.ru>
2024-04-12 16:00:18 +0100

    21sprintStart

--- 20sprint/unit_20/unit_20.js
+++ 20sprint/unit_20/unit_20.js
@@ -4,6 +4,7 @@
 
 function t1(event) {
     console.log(event);
+    document.querySelector('.out-1').innerHTML = event.key;
 }
 
 document.querySelector('.i-1').onkeydown = t1;
@@ -13,7 +14,7 @@ document.querySelector('.i-1').onkeydown = t1;
 
 function t2(event) {
     console.log(event);
-
+    document.querySelector('.out-2').innerHTML = event.keyCode;
 }
 
 document.querySelector('.i-2').onkeypress = t2;
@@ -23,7 +24,12 @@ document.querySelector('.i-2').onkeypress = t2;
 
 
 function t3(event) {
-
+    if (event.keyCode >= 57) {
+        return document.querySelector('.out-3').innerHTML = true;
+    }
+    else {
+        return document.querySelector('.out-3').innerHTML = false;
+    }
 }
 
 document.querySelector('.i-3').onkeypress = t3;
@@ -34,6 +40,14 @@ document.querySelector('.i-3').onkeypress = t3;
 
 function t4(event) {
     console.log(event.keyCode);
+    let str = event.keyCode
+    if (str < 97) {
+        str += 32
+        console.log(str)
+    }
+    let lowSting = String.fromCharCode(str)
+    document.querySelector('.out-4').innerHTML = lowSting;
+
 
 }
 
@@ -44,19 +58,29 @@ document.querySelector('.i-4').onkeypress = t4;
 
 function t5(event) {
     console.log(event);
-
+    let str = event.keyCode;
+    if (str >= 97) {
+        str -= 32;
+    }
+    let bigString = String.fromCharCode(str);
+    console.log(bigString)
+    document.querySelector('.out-5').innerHTML = bigString;
 }
 
 document.querySelector('.i-5').onkeydown = t5;
 
 // Task 6 ============================================
 /*  Дан input .i-6. Напишите функцию t6, которая позволяет вводить в i-6 только символы в нижнем регистре. Т.е. мы до функции создаем строку. Внутри функции проверяем код символа. Если символ - в нижнем регистре - добавляем в строку. Потом принудительно присваиваем строку в value input i-6. Чтобы блокировать стандартный вывод в input в конце функции пишем return false;  */
-
-function t6() {
-    // return false;
+let str = '';
+function t6(event) {
+    let keyCode = event.keyCode;
+    if (keyCode >= 97 && keyCode <= 122) {
+        str += String.fromCharCode(keyCode);
+    }
+    document.querySelector('.i-6').value = str;
+    return false;
 }
-
-document.querySelector('.i-6').onkeypress= t6;
+document.querySelector('.i-6').onkeypress = t6;
 
 
 // Task 7 ============================================
@@ -65,34 +89,47 @@ document.querySelector('.i-6').onkeypress= t6;
 const a7 = ['a', 'z', 'x', 'w', 'y', 't'];
 
 function t7() {
-
+    let count = Math.floor(Math.random() * a7.length);
+    document.querySelector('.out-7').innerHTML = a7[count];
 }
 
-document.querySelector('.i-7').onkeydown= t7;
+document.querySelector('.i-7').onkeydown = t7;
 
 // Task 8 ============================================
 /*  Дан input .i-8. Напишите функцию t8, которая дописывает в .out-8 вводимый в input текст, но заменяет i на 1, o на 0, l на 7. */
 
 const a8 = {
-    i : 1,
-    o : 0,
-    l : 7
+    i: 1,
+    o: 0,
+    l: 7
 }
 
 function t8(event) {
     // 1. Получаем из event введенный символ
+    let str = event.key;
+    let out = document.querySelector('.out-8');
+    for (let key in a8) {
+        if (str === key) {
+            str = a8[key];
+        }
+    }
+    out.innerHTML = str;
     // 2. Проверяем наличие такого ключа в a8 - уже делали это раньше!!!!
     // 3. Если есть дописываем в out-8 символ из массива a8. Если нет - введенный символ.
 }
 
-document.querySelector('.i-8').onkeydown= t8;
+document.querySelector('.i-8').onkeydown = t8;
 
 
 // Task 9 ============================================
 /* Дан input .i-9. Напишите функцию t9, выводит в .out-9 количество (число) нажатых клавиш стрелка вниз. */
-
+count = 0
 function t9(event) {
     console.log(event);
+    if (event.key === 'ArrowDown') {
+        document.querySelector('.out-9').innerHTML = count++;
+    }
+
 }
 
 document.querySelector('.i-9').onkeydown = t9;
@@ -105,7 +142,17 @@ let h = 75;
 let w = 75;
 
 function t10(event) {
-    // увеличиваем  h, w, потом присваиваем как свойства...
+    console.log(event.key)
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+        w += 1
+        document.querySelector('.block-10').style.width = w + 'px';
+    }
+    if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+        h += 1
+        document.querySelector('.block-10').style.height = h + 'px';
+    }
+
+
 
 }
 
@@ -121,7 +168,17 @@ document.querySelector('.i-10').onkeydown = t10;
 */
 
 function t11(event) {
-    console.log(event.key);
+    let keyW = document.getElementsByClassName('keyboard');
+    let value = ''
+    for (item of keyW) {
+        item.classList.remove('active')
+    }
+    for (item of keyW) {
+        value = item.getAttribute('data')
+        if (event.key === value) {
+            item.classList.add('active');
+        }
+    }
 
 }
 

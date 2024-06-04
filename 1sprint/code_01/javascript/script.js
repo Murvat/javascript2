7e5b3fa2800c794b78cd18b15ff8c67420d435f4
Murvat <murvat.mustafayev@mail.ru>
2024-06-04 18:31:39 +0100

    .

--- 1sprint/code_01/javascript/script.js
+++ 1sprint/code_01/javascript/script.js
@@ -10,19 +10,17 @@ console.info('Hello');
 // alert('Hello');
 // alert('Hello'); // ctrl+l
 // alert('Hello');
-
 document.getElementById('out').innerHTML = 'Hello';
-document.getElementById('out').innerHTML = 2019;
+document.getElementById('out').innerHTML = String(2019);
 document.getElementById('out').innerHTML = '<b>2020</b>';
-
-document.querySelector('.header').innerHTML = 15;
-document.querySelector('#one').innerHTML = 777; // есть #
-document.getElementById('one').innerHTML = 888; // нет #
-
-let b; // var a - устарело
-let a = document.querySelector('#one'); // внутрь a получил параграф
-let c;
-c = document.querySelector('.header');
-
-a.innerHTML = 9999; // new! - ошибка 
-c.innerHTML = 6666;
+export {};
+// document.querySelector('.header').innerHTML = 15;
+// document.querySelector('#one').innerHTML = 777; // есть #
+// document.getElementById('one').innerHTML = 888; // нет #
+// let b; // var a - устарело
+// let a = document.querySelector('#one'); // внутрь a получил параграф
+// let c;
+// c = document.querySelector('.header');
+// a.innerHTML = 9999; // new! - ошибка
+// c.innerHTML = 6666;
+//# sourceMappingURL=script.js.map
